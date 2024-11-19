08fabb9fdbc7d40dde58261db4553d1767ccb3ab
HARRISHAJA <mohamed.harris709@gmail.com>
2024-11-19 20:33:01 +0530

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo off
+echo.
+echo  mohamed harris     
+echo.
+echo Installing required Python packages...
+pip install -r requirements.txt
+echo Installation complete.
+pause
---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo off
+echo.
+echo  mohamed harris      
+echo.
+echo Starting  application...
+python app.py
+pause
